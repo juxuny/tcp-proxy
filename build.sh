198b7ba45357c655d34a3f9a08e65ac3933e93f7
Juxuny Wu <juxuny@163.com>
2023-04-02 20:25:22 +0800

    health check daemon

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-GOOS=linux GOHOSTARCH=amd64 GOARCH=amd64 CGO_ENABLED=0 go build -o ./build/tcp-proxy_linux_x64_v1.0.7
+GOOS=linux GOHOSTARCH=amd64 GOARCH=amd64 CGO_ENABLED=0 go build -o ./build/tcp-proxy_linux_x64_v1.1.0
